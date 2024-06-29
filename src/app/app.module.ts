50c8e572f8009c7a9e1fd91dced774e0b950979e
Shibaji Debnath <imshibaji@gmail.com>
2024-06-29 16:07:35 +0530

    Added Pages and Router Settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,37 @@ import { AppComponent } from './app.component';
 import { ThemeModeBtnComponent } from './common/theme-mode-btn/theme-mode-btn.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ThemeModeBtnComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    CartComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'product/:id', component: ProductComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  menus: any[] = [
+    {
+      title: 'Home',
+      link: '/',
+    },
+    {
+      title: 'About',
+      link: '/about',
+    },
+    {
+      title: 'Products',
+      link: '/products',
+      children: [
+        {
+          title: 'Submenu 1',
+          link: '/submenu1',
+        },
+        {
+          title: 'Submenu 2',
+          link: '/submenu2',
+        },
+      ],
+    },
+    {
+      title: 'Contact',
+      link: '/contact',
+    },
+    {
+      title: 'Cart',
+      link: '/cart',
+    },
+  ]
 }
